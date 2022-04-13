e5d15f6a5892655c94521d2c825a45a65799c063
ZC <zhoucan1999@outlook.com>
2022-04-13 20:20:26 +0800

    Update Widths

--- widths.py
+++ widths.py
@@ -6,7 +6,7 @@ from utilities import *
 
 ###########################################################################################################
 ## Random network examples
-def RomdomExperiment01(experimentIndex, inputSize):
+def RomdomExperiment01(experimentIndex, inputSize, hiddenSize):
     radius = [0.00000001, 0, 0] # [input, weight, bias]
     outputIndex = 0
     c_vector = [1, 0]
@@ -14,7 +14,7 @@ def RomdomExperiment01(experimentIndex, inputSize):
     stop_conditions = [1e-6, 2, 10000, 10] #[minGap, maxiteration, maxBoxes, maxSame]
     needBisect = True
 
-    layer_sizes = [inputSize, inputSize, 2]
+    layer_sizes = [inputSize, hiddenSize, 2]
     input = [0.5 for x in range(inputSize)]
 
     ept = Experiments(radius, layer_sizes, outputIndex, stop_conditions, needBisect, experimentIndex, input, dataIndex, c_vector)
@@ -29,7 +29,7 @@ def RomdomExperiment01(experimentIndex, inputSize):
     ept.OtherMethods()
 
     results = ""
-    # results += "interval ReLU: value - " + str(ept.interval_result_ReLU) + " timing - " + str(ept.interval_time_ReLU) + "\n"
+    results += "interval ReLU: value - " + str(ept.interval_result_ReLU) + " timing - " + str(ept.interval_time_ReLU) + "\n"
     # results += "lipMIP ReLU: value - " + str(ept.lipMIP_result) + " timing - " + str(ept.lipMIP_time) + "\n"
     # results += "ZLip ReLU: value - " + str(ept.ZLip_result) + " timing - " + str(ept.ZLip_time) + "\n"
     results += "CLEVER ReLU: value - " + str(ept.otherMethodsResults[0][1]) + " timing - " +  str(ept.otherMethodsResults[0][0]) + "\n"
@@ -39,11 +39,16 @@ def RomdomExperiment01(experimentIndex, inputSize):
     # results += "SeqLip ReLU: value - " + str(ept.otherMethodsResults[4][1]) + " timing - " + str(ept.otherMethodsResults[4][0]) + "\n\n"
 
     print(results)
-    ept.saveNetwork(folderName="MyRandom", networkName=str(experimentIndex))
-    ept.storeExperiment(folderName="MyRandom")
+    ept.saveNetwork(folderName="Widths", networkName=str(experimentIndex))
+    ept.storeExperiment(folderName="Widths")
 
 
 
 ###########################################################################################################
 ###########################################################################################################
-RomdomExperiment01(experimentIndex = 10, inputSize = 4)
+
+for i in range(10):
+    hiddenNode = pow(2, i+1)
+    print("The number of hidden node is " + str(hiddenNode))
+    RomdomExperiment01(experimentIndex = 1, inputSize = 2, hiddenSize = hiddenNode)
+    print("##########################################################################\n")
